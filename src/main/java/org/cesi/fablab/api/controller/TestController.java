45ac7d048423b4136ca628a8e0ddcbd6d2daa236
MaximeL50 <djidane.lebiez@laposte.net>
2021-01-07 10:27:32 +0100

    Convert Project to Maven

---
+++ src/main/java/org/cesi/fablab/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.cesi.fablab.api.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Bienvenue sur l'application FabLab!";
+	}
+
+}
