d4cec53e0b7d4971ee657d1ff717ef4026f78387
BottonL <botton.lea@gmail.com>
2021-01-09 17:18:02 +0100

    add DeliveryStatusDTO, DeliveryStatusRepository, DeliveryStatusService and DeliveryStatusServiceImpl

---
+++ src/main/java/org/cesi/fablab/api/dto/DeliveryStatusDTO.java
@@ -0,0 +1,30 @@
+package org.cesi.fablab.api.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cesi.fablab.api.entity.DeliveryStatusEntity;
+import org.cesi.fablab.api.entity.PurchaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryStatusDTO {
+	private int id;
+	private String name;
+	private List<PurchaseEntity> purchasesList = new ArrayList<PurchaseEntity>();
+
+	public DeliveryStatusDTO(final DeliveryStatusEntity deliveryStatusEntity) {
+		super();
+		this.id = deliveryStatusEntity.getId();
+		this.name = deliveryStatusEntity.getName();
+		// this.purchasesList = deliveryStatusEntity.getPurchasesList();
+	}
+
+}
