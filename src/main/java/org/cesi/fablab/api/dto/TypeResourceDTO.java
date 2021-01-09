2906429016e10a03824e59c948d6283a836c5b2c
BottonL <botton.lea@gmail.com>
2021-01-09 14:57:51 +0100

    add TypeResourceDTO, TypeResourceRepository, TypeResourceService and TypeResourceServiceImpl

---
+++ src/main/java/org/cesi/fablab/api/dto/TypeResourceDTO.java
@@ -0,0 +1,30 @@
+package org.cesi.fablab.api.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cesi.fablab.api.entity.ResourceEntity;
+import org.cesi.fablab.api.entity.TypeResourceEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeResourceDTO {
+	private int id;
+	private String name;
+	private List<ResourceEntity> resourcesList = new ArrayList<ResourceEntity>();
+
+	public TypeResourceDTO(final TypeResourceEntity typeResourceEntity) {
+		super();
+		this.id = typeResourceEntity.getId();
+		this.name = typeResourceEntity.getName();
+		// this.resourcesList = typeResourceEntity.getResourcesList();
+	}
+
+}
