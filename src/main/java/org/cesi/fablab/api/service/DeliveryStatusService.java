77a227180ac8350e6172fd9eb208bcb325c3f62f
BottonL <botton.lea@gmail.com>
2021-01-09 17:18:02 +0100

    add DeliveryStatusDTO, DeliveryStatusRepository, DeliveryStatusService and DeliveryStatusServiceImpl

---
+++ src/main/java/org/cesi/fablab/api/service/DeliveryStatusService.java
@@ -0,0 +1,16 @@
+package org.cesi.fablab.api.service;
+
+import java.util.List;
+
+import org.cesi.fablab.api.dto.DeliveryStatusDTO;
+
+public interface DeliveryStatusService {
+	List<DeliveryStatusDTO> getAllDeliveryStatus() throws Exception;
+
+	void addDeliveryStatus(DeliveryStatusDTO dto) throws Exception;
+
+	void removeDeliveryStatus(DeliveryStatusDTO dto) throws Exception;
+
+	void updateDeliveryStatus(DeliveryStatusDTO dto) throws Exception;
+
+}
---
+++ src/main/java/org/cesi/fablab/api/service/DeliveryStatusServiceImpl.java
@@ -0,0 +1,48 @@
+package org.cesi.fablab.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cesi.fablab.api.dto.DeliveryStatusDTO;
+import org.cesi.fablab.api.entity.DeliveryStatusEntity;
+import org.cesi.fablab.api.repository.DeliveryStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeliveryStatusServiceImpl implements DeliveryStatusService {
+	@Autowired
+	private DeliveryStatusRepository deliveryStatusRepository;
+
+	@Override
+	public List<DeliveryStatusDTO> getAllDeliveryStatus() throws Exception {
+		List<DeliveryStatusDTO> lstDeliveryStatusDTO = new ArrayList<DeliveryStatusDTO>();
+		List<DeliveryStatusEntity> lstDeliveryStatusEntity = deliveryStatusRepository.findAll();
+		if (lstDeliveryStatusEntity != null && !lstDeliveryStatusEntity.isEmpty()) {
+			for (DeliveryStatusEntity currentDeliveryStatusEntity : lstDeliveryStatusEntity) {
+				DeliveryStatusDTO deliveryStatusDTO = new DeliveryStatusDTO(currentDeliveryStatusEntity);
+				lstDeliveryStatusDTO.add(deliveryStatusDTO);
+			}
+		}
+		return lstDeliveryStatusDTO;
+	}
+
+	@Override
+	public final void addDeliveryStatus(final DeliveryStatusDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public final void removeDeliveryStatus(final DeliveryStatusDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public final void updateDeliveryStatus(final DeliveryStatusDTO dto) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
