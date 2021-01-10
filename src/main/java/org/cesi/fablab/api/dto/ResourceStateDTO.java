48a1d80dbf91ebfa3dee9fe8c90362ea54c462b2
BottonL <botton.lea@gmail.com>
2021-01-10 16:12:28 +0100

    add ResourceStateDTO, ResourceStateRepository, ResourceStateService and ResourceStateServiceImpl

---
+++ src/main/java/org/cesi/fablab/api/dto/ResourceStateDTO.java
@@ -0,0 +1,30 @@
+package org.cesi.fablab.api.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cesi.fablab.api.entity.ResourceEntity;
+import org.cesi.fablab.api.entity.ResourceStateEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResourceStateDTO {
+	private int id;
+	private String name;
+	private List<ResourceEntity> resourceList = new ArrayList<ResourceEntity>();
+
+	public ResourceStateDTO(final ResourceStateEntity resourceStateEntity) {
+		super();
+		this.id = resourceStateEntity.getId();
+		this.name = resourceStateEntity.getName();
+		// this.resourceList = resourceStateEntity.getResourceList();
+	}
+
+}
