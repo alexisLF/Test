e228788caae8b307386e8dc39416e4db5aeae8f5
BottonL <botton.lea@gmail.com>
2021-01-17 15:06:29 +0100

    fix cors error with angular app

--- src/main/java/org/cesi/fablab/FablabApplication.java
+++ src/main/java/org/cesi/fablab/FablabApplication.java
@@ -2,11 +2,24 @@ package org.cesi.fablab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FablabApplication {
 
     public static final void main(final String[] args) {
         SpringApplication.run(FablabApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
