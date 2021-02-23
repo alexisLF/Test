2c2fafa42571e58c3448b460ce048366f1995976
BottonL <botton.lea@gmail.com>
2021-02-23 14:12:49 +0100

    change cors origin permission (temporary)

--- src/main/java/org/cesi/fablab/FablabApplication.java
+++ src/main/java/org/cesi/fablab/FablabApplication.java
@@ -17,9 +17,9 @@ public class FablabApplication {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT",
-                        "PATCH", "DELETE", "OPTIONS");
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE",
+                        "OPTIONS");
             }
         };
     }
